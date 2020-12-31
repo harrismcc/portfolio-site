f2e8cc276963ddd744eb4431366024d78ee4aa18
Harris McCullers <harrismcc@gmail.com>
2020-12-30 19:36:00 -0500

    Social buttons are in place, still have an issue with the color on click

--- src/components/bio.js
+++ src/components/bio.js
@@ -42,27 +42,13 @@ const Bio = () => {
 
   const avatar = data?.avatar?.childImageSharp?.fixed
 
-
-  const AnimatedImage = animated(Image)
-
-  const [props, set] = useSpring(() => ({minWidth: 50, height: 50, borderRadius: "50%"}))
-  const handleBioHover = () => {
-    set({minWidth: 100, height: 100, borderRadius: "50%"})
-  }
-  const handleBioUnhover = () => {
-    set({minWidth: 50, height: 50, borderRadius: "10%"})
-  }
-
   return (
     <div 
-      roll="button"
+      roll="display"
       className="bio"
-      onMouseEnter={() => handleBioHover()}
-      onMouseLeave={() => handleBioUnhover()}
     >
       {avatar && (
-        <AnimatedImage
-          style={props}
+        <Image
           fixed={avatar}
           alt={author?.name || ``}
           className="bio-avatar"
@@ -71,17 +57,88 @@ const Bio = () => {
           }}
         />
       )}
-      {author?.name && (
-        <p>
-          <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
-        </p>
-      )}
+      <div>
+        <small className="bio-location-tag">Software Engineer - Los Angeles, California</small>
+        {author?.name && (
+          <p>
+            <strong>{author.name}</strong> {author?.summary || null}
+
+          </p>
+        )}
+      <Socials/>
+      </div>
+
     </div>
   )
 }
 
 export default Bio
+
+
+
+//linkedin link
+// github link
+// email link
+// resume link
+
+const Socials = () => {
+
+  return(
+      <div className="social-display">
+          <SocialButton
+              className="A"
+              buttonText="Github"
+              src="https://github.com/harrismcc/"
+          />
+          <SocialButton
+              className="B"
+              buttonText="LinkedIn"
+              src="https://www.linkedin.com/in/harrismccullers/"
+          />
+          <SocialButton
+              buttonText="Resume"
+              src="https://github.com/harrismcc/"
+          />
+          <SocialButton
+              buttonText="Email"
+              src="mailto://harrismccullers@gmail.com"
+          />
+      </div>
+  )
+}
+
+
+const SocialButton = (props) => {
+
+  const handleClick = () => {
+    //window.location.href=props.src;
+    window.open(
+      props.src,
+      '_blank' // <- This is what makes it open in a new window.
+    );
+  }
+
+  const [springStyle, set] = useSpring(() => ({paddingLeft: "5px", paddingRight: "5px", letterSpacing: "1px"}))
+  const handleMouseEnter = () => {
+    console.log("Enter")
+    set({paddingLeft: "8px",
+          paddingRight: "8px",
+          letterSpacing: "2.5px"})
+  }
+  const handleMouseLeave = () => {
+    set({paddingLeft: "5px",
+    paddingRight: "5px",
+    letterSpacing: "1px"})
+  }
+  return(
+      <animated.button 
+        style={springStyle}
+        className="social-button"
+        onClick={handleClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+          {props.buttonText}
+      </animated.button>
+  )
+}
