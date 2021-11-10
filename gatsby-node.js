34b2640d61570f39c24b6f841bac4205971cc52b
Harris McCullers <harrismcc@gmail.com>
2021-11-09 18:12:06 -0800

    seems to be working

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,10 +6,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
 
   // Define a template for blog post
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const workPost = path.resolve(`./src/templates/work-post.js`)
-
-  
+  const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
 
   // Get all markdown blog posts sorted by date
   const result = await graphql(
@@ -44,29 +41,24 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
   // `context` is available in the template as a prop and as a variable in GraphQL
-
   if (posts.length > 0) {
-    posts.forEach((post, index) => {
-      let postFormat = blogPost
-      if (post.fields.collection == `work`){
-        postFormat = workPost
+    const blogPosts = posts.filter((post) => post.fields.collection !== 'work')
+    blogPosts.forEach((post, index) => {
+
+      const previousPostId = index === 0 ? null : blogPosts[index - 1].id
+      const nextPostId = index === blogPosts.length - 1 ? null : blogPosts[index + 1].id
+
+      if (post.fields.collection !== 'work'){
+        createPage({
+          path: post.fields.slug,
+          component: blogPostTemplate,
+          context: {
+            id: post.id,
+            previousPostId,
+            nextPostId,
+          },
+        })
       }
-
-      const previousPostId = index === 0 ? null : posts[index - 1].id
-      const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
-
-      createPage({
-        path: post.fields.slug,
-        component: postFormat,
-        context: {
-          id: post.id,
-          previousPostId,
-          nextPostId,
-        },
-      })
-      
-
-
     })
   }
 }
